fb70d74e73b17f9d4773c6674324081a9db55378
kerba <fluxoid@gmail.com>
2025-10-22 21:03:32 +0300

    обновлен readme.md и в домены добавлен pmant.kerba.net

--- frontend-app/vite.config.ts
+++ frontend-app/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 3000,
+    allowedHosts: [ "pmant.kerba.net", "localhost" ]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
