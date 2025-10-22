796ad88c7b8ad129e99664aad5655ea513fb0448
kerba <fluxoid@gmail.com>
2025-10-22 23:37:34 +0300

    отладка wait-for-cert.sh

--- nginx/wait-for-cert.sh
+++ nginx/wait-for-cert.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-# wait-for-cert.sh
-
 DOMAIN="pmant.kerba.net"
 CERT_DIR="/etc/letsencrypt/live/$DOMAIN"
-
-# Максимальное время ожидания в секундах
 TIMEOUT=300
 INTERVAL=5
 ELAPSED=0
 
-echo "Ждём сертификат для $DOMAIN..."
 
 while [ ! -f "$CERT_DIR/fullchain.pem" ] || [ ! -f "$CERT_DIR/privkey.pem" ]; do
-    if [ $ELAPSED -ge $TIMEOUT ]; then
+    echo "Ждём сертификат для $DOMAIN в течение $ELAPSED sec ..."
+    if [ "$ELAPSED" -ge "$TIMEOUT" ]; then
         echo "Timeout: сертификат не появился за $TIMEOUT секунд"
         exit 1
     fi
