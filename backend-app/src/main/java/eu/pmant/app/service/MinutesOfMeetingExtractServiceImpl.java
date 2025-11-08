f8c3adc90e5545fecf6eaa3f3c21a8f6f3970505
kerba <fluxoid@gmail.com>
2025-11-08 17:48:00 +0300

    Эксперименты с вызовом тулов

--- backend-app/src/main/java/eu/pmant/app/service/MinutesOfMeetingExtractServiceImpl.java
+++ backend-app/src/main/java/eu/pmant/app/service/MinutesOfMeetingExtractServiceImpl.java
@@ -2,22 +2,28 @@ package eu.pmant.app.service;
 
 import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
 import com.openai.models.responses.*;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 @Service
 public class MinutesOfMeetingExtractServiceImpl implements MinutesOfMeetingExtractService {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public Optional<String> extractMinutesOfMeeting(String meetingText) {
@@ -38,21 +44,54 @@ public class MinutesOfMeetingExtractServiceImpl implements MinutesOfMeetingExtra
         Response response = client.responses().create(builder.build());
         log.info("response: {}", response);
 
+        // Поиск и логирование tool calls, если есть (доступно — зависит от модели и вашей интеграции)
+        List<ResponseOutputItem> functionShouldBeCalledByModelDecision = response.output().stream()
+            .filter(ResponseOutputItem::isFunctionCall)
+            .toList();
+        functionShouldBeCalledByModelDecision.forEach(callFunctionConsumer(inputs));
+
+        if (CollectionUtils.isNotEmpty(functionShouldBeCalledByModelDecision)) {
+            try {
+                log.info("Модель решила вызвать tools: {}", OBJECT_MAPPER.writeValueAsString(functionShouldBeCalledByModelDecision));
+                response = client.responses().create(builder.build());
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            log.info("Модель не приняла решения вызвать tools");
+        }
+
         // Разбор текста
         String textOutput = response.output().stream()
+            .filter(ResponseOutputItem::isMessage)
             .flatMap(outputItem -> outputItem.message().stream())
             .flatMap(item -> item.content().stream())
             .flatMap(content -> content.outputText().stream())
             .flatMap(outputItem -> Stream.of(outputItem.text()))
             .collect(Collectors.joining());
 
-        // Поиск и логирование tool calls, если есть (доступно — зависит от модели и вашей интеграции)
-        response.output().stream()
-            .filter(ResponseOutputItem::isFunctionCall)
-            .flatMap(outputItem -> outputItem.functionCall().stream())
-            .forEach(functionToolCall -> log.info("functionToolCall: {}", functionToolCall));
-
         return Optional.ofNullable(textOutput);
+    }
+
+    @NotNull
+    private Consumer<ResponseOutputItem> callFunctionConsumer(List<ResponseInputItem> inputs) {
+        return functionCall -> {
+            ResponseFunctionToolCall functionToolCall = functionCall.asFunctionCall();
+            inputs.add(ResponseInputItem.ofFunctionCall(functionToolCall));
+            inputs.add(ResponseInputItem.ofFunctionCallOutput(ResponseInputItem.FunctionCallOutput.builder()
+                .callId(functionToolCall.callId())
+                .outputAsJson(callFunction(functionToolCall))
+                .build()));
+        };
+    }
+
+    private static Object callFunction(ResponseFunctionToolCall function) {
+        switch (function.name()) {
+            case "create_trello_card":
+                return function.arguments(CreateTrelloActionItem.class).execute();
+            default:
+                throw new IllegalArgumentException("Unknown function: " + function.name());
+        }
     }
 
     @Slf4j
@@ -69,8 +108,9 @@ public class MinutesOfMeetingExtractServiceImpl implements MinutesOfMeetingExtra
         @JsonPropertyDescription("Срок выполнения (если указан)")
         private String dueDate;
 
-        public void execute() {
-            log.info("Вызвали tool CreateTrelloActionItem ({}, {}, {})", name, description, dueDate);
+        public boolean execute() {
+            log.info("Вызов функции CreateTrelloActionItem ('{}', '{}', '{}')", name, description, dueDate);
+            return true;
         }
     }
 }
